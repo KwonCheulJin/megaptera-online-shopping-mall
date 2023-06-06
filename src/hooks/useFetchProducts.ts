e278dbb0db66148f31abb7724b4d4b6fefc30cc8
jin <chkftm12@gmail.com>
2023-06-06 13:59:01 +0900

    - feat: 삼품목록 개발 완료

---
+++ src/hooks/useFetchProducts.ts
@@ -0,0 +1,17 @@
+import { container } from 'tsyringe';
+import { useEffect } from 'react';
+import { useStore } from 'usestore-ts';
+import ProductsStore from '../stores/ProductsStore';
+
+export default function useFetchProducts() {
+  const store = container.resolve(ProductsStore);
+  const [{ products }] = useStore(store);
+
+  useEffect(() => {
+    store.fetchProducts();
+  }, []);
+
+  return {
+    products,
+  };
+}
